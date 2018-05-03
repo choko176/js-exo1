3dc68a2c411a6811a8f263eb0c66696b5669a6ed
choko176 <akuetevi.edmond@gmail.com>
2018-05-03 23:07:16 +0200

    first commit

---
+++ toto.js
@@ -0,0 +1,38 @@
+var text    = document.querySelector("#text");
+var coude    = document.querySelector("#coude");
+var poignet    = document.querySelector("#poignet");
+var hanche    = document.querySelector("#hanche");
+var genou    = document.querySelector("#genou");
+var cheville    = document.querySelector("#cheville");
+var epaule    = document.querySelector("#epaule");
+
+
+epaule.addEventListener("mouseover", function(){
+text.innerHTML = " L'Epaule : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+
+});
+
+coude.addEventListener("mouseover", function(){
+text.innerHTML = " Le Coude : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+
+});
+
+poignet.addEventListener("mouseover", function(){
+text.innerHTML = " Le Poignet : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+
+});
+
+hanche.addEventListener("mouseover", function(){
+text.innerHTML = " La Hanche : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+
+});
+
+genou.addEventListener("mouseover", function(){
+text.innerHTML = " Le Genou : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+
+});
+
+cheville.addEventListener("mouseover", function(){
+text.innerHTML = " La Cheville : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+
+});
