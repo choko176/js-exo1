4a23ac797007662eddd00c2837fbe5f68c8a907c
choko176 <akuetevi.edmond@gmail.com>
2018-05-15 13:15:24 +0200

    mise a jour texte

--- toto.js
+++ toto.js
@@ -8,27 +8,27 @@ var epaule    = document.querySelector("#epaule");
 
 
 epaule.addEventListener("mouseover", function(){
-text.innerHTML = " L'Epaule : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+text.innerHTML = " L'Epaule : <br/><br/><br/>L’épaule se compose de trois os, la clavicule, la scapula ou omoplate et l’humérus.La mobilité de l'épaule est assurée principalement par le muscle deltoïde, qui apporte la puissance, et par les muscles de la coiffe, qui initient le mouvement et stabilisent la tête de l'humérus.La coiffe des rotateurs est le nom donné à l'ensemble des tendons qui forment un 'chapeau' au-dessus de la tête de l'humérus. Entre la tête de l'humérus et l'acromion (excroissance de la scapula), se trouve une bourse de glissement appelée bourse sous-acromiale. ";
 
 });
 
 coude.addEventListener("mouseover", function(){
-text.innerHTML = " Le Coude : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+text.innerHTML = " Le Coude : <br/><br/><br/>Le coude est une articulation du membre supérieur qui joint le bras à l'avant-bras, et en particulier trois os : l'humérus, le radius et le cubitus. Aussi appelé articulation olécranienne, le coude est composé de trois articulations distinctes, qui permettent différents mouvements du bras : flexion/extension, rotation, pronation et supination. Les pathologies du coude sont courantes (luxation, tendinite... ) mais se déclarent le plus souvent chez les personnes âgées ayant des problèmes d'arthrose, qui affectent les cartilages. ";
 
 });
 
 poignet.addEventListener("mouseover", function(){
-text.innerHTML = " Le Poignet : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+text.innerHTML = " Le Poignet : <br/><br/><br/>Le poignet est une articulation qui se situe entre la main et l'avant-bras. Cette articulation permet les mouvements de la main par rapport à l'avant-bras. Anatomiquement, le poignet comprend 10 os. Il englobe le radius et l'ulna (cubitus) dans leur partie basse. Dans la première rangée, on trouve le scaphoïde, le lunatum (semi-lunaire), le triquénum (pyramidal) et le pisiforme. La deuxième rangée comprend l'hamatum (os crochu), le capitatum (grand os), le trapézoïde et le trapèze. ";
 
 });
 
 hanche.addEventListener("mouseover", function(){
-text.innerHTML = " La Hanche : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+text.innerHTML = " La Hanche : <br/><br/><br/>La hanche (issu du mot germanique hanka,qui a remplacé le terme coxa en latin, os de la hanche) constitue l’articulation unissant le membre inférieur à la ceinture pelvienne, au niveau du bassin . ";
 
 });
 
 genou.addEventListener("mouseover", function(){
-text.innerHTML = " Le Genou : <br/><br/><br/>Exsistit autem hoc loco quaedam quaestio subdifficilis, num quando amici novi, digni amicitia, veteribus sint anteponendi, ut equis vetulis teneros anteponere solemus. Indigna homine dubitatio! Non enim debent esse amicitiarum sicut aliarum rerum satietates; veterrima quaeque, ut ea vina, quae vetustatem ferunt, esse debet suavissima; verumque illud est, quod dicitur, multos modios salis simul edendos esse, ut amicitiae munus expletum sit. ";
+text.innerHTML = " Le Genou : <br/><br/><br/>Le genou est une articulation qui relie le fémur (qui supporte la cuisse) au tibia (qui supporte la jambe). Sur son extrémité inférieure, le fémur présente deux protubérances, les condyles. Ces condyles s’articulent en regard de deux plateaux sur l’extrémité supérieure du tibia. Lorsqu’on plie le genou, les condyles du fémur roulent et glissent en même temps sur les plateaux du tibia. Dans cette articulation, un troisième os joue un rôle essentiel : la rotule (ou patella) qui est située à l’avant du genou. Elle est maintenue par deux tendons, le tendon du quadriceps et le tendon rotulien. Ils l’attachent en haut au muscle de la cuisse (le quadriceps) et en bas à l’os du tibia. La rotule glisse en avant niveau du fémur, dans une travée qui s’appelle la trochlée. L’ensemble rotule-tendons permet de transmettre de la force aux muscles.";
 
 });
 
